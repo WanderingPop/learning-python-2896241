9f89a29e5a28c7982ca8522a5b00b09c9bb4bf87
Chris <83107230+WanderingPop@users.noreply.github.com>
2022-11-20 17:26:35 -0700

    Update helloworld_start.py

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,9 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
-
+def main():
+    print("hello world")
+    name = input("what is your name?")
+    print("Nice to meet you", name)
+if __name__ == "__main__":
+    main()
